099b36a3b42f87fc401c3177db65369de61f915d
bleck09 <blady12345tkm@gmail.com>
2025-12-07 00:15:22 -0400

    cambiando de home

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,56 +1,28 @@
 import React from "react";
 import "./Home.css";
-import Folder from "../../secciones/folder/Folder";
 import TextoDesvanecido from "../../secciones/textos/textoDesvanecido/TextoDesvanecido";
-import BotonInvitacion from "../../secciones/botonInvitacion/BotonInvitacion";
+
+import Sobre from "../../secciones/sobre/Sobre";
 
 const Home = () => {
-
-  // Obtener parámetros desde el URL
-  const params = new URLSearchParams(window.location.search);
-  const invitadoID = params.get("i"); // enlace corto: ?i=
-
-  // Diccionario de invitados
-  const invitados = {
-    juan: "Sr. Santiago",
-    maria: "Sra. Maria",
-    chambi: "Familia Chambi",
-    padrinos: "Mis Padrinos",
-    general: "Público general"
-  };
-
-  // Si existe en el diccionario, lo usamos — si no, usamos "Público general"
-  const nombreInvitado = invitados[invitadoID] || invitados.general;
-
-  const tituloInvitacion = "Invitación para:";
-
   return (
     <div className="fondo_invitacion">
       <div className="overlay">
-        <TextoDesvanecido 
-          text="Mis 15 Años" 
-          delay={500} 
-          animateBy="words" 
-          className="Titulo_adriana_Principal" 
+        <TextoDesvanecido
+          text="Mi Primera Comunión"
+          delay={500}
+          animateBy="words"
+          className="Titulo_luis_Principal"
         />
-
-        <br /><br /><br /><br /><br />
-
-        <div className="home-container">
-          <Folder
-            size={2}
-            className="custom-folder"
-            color="var(--rosa-empolvado)"
-            var_invitado={tituloInvitacion}
-            var_nombre_invitado={nombreInvitado}
-          />
-        </div>
-
-        <h2 className="mensaje_invitacion">
-          Tenemos el honor de invitarle a celebrar los 15 años de nuestra hija.
-        </h2>
-
-        <BotonInvitacion />
+       
+        <br /><br /><br />
+        <Sobre />
+         <TextoDesvanecido
+          text="Luis Miguel"
+          delay={100}
+          animateBy="words"
+          className="Titulo_luis_Principal"
+        />
       </div>
     </div>
   );
